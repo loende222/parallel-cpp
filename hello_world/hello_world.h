c788dcee0db725b5dae22211aeaca5d8cc8d4f69
Aliya Zaripova <aliyazaripova@MacBook-Pro-Aliya.local>
2021-07-03 19:15:22 +0300

    Corrected solution

--- hello_world/hello_world.h
+++ hello_world/hello_world.h
@@ -9,26 +9,25 @@
 
 class HelloWorld {
 private:
-    std::vector<std::thread> Threads;
-    size_t kThreads;
-    std::mutex mtx;
+    const size_t n_threads_;
+    std::vector<std::thread> threads_;
+    std::mutex mtx_;
 
 public:
     static constexpr std::string_view kHelloPrefix = "Hello, World! From thread ";
 
-    HelloWorld(size_t n_threads) : kThreads(n_threads) {
+    HelloWorld(size_t n_threads) : n_threads_(n_threads) {
     }
 
     void SayHello(std::ostream& os) {
-        for (size_t i = 0; i != kThreads; ++i) {
-            Threads.emplace_back([&] {
-                mtx.lock();
+        for (size_t i = 0; i != n_threads_; ++i) {
+            threads_.emplace_back([&] {
+                std::unique_lock lock(mtx_);
                 os << kHelloPrefix << std::this_thread::get_id() << "\n";
-                mtx.unlock();
             });
         }
-        for (size_t i = 0; i != kThreads; ++i) {
-            Threads[i].join();
+        for (auto& t : threads_) {
+            t.join();
         }
     }
 };
