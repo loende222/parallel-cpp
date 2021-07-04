2d5b55cad559e6bd3adf58507c265d6f83b582ce
Aliya Zaripova <aliyazaripova@MacBook-Pro-Aliya.local>
2021-07-04 18:53:43 +0300

    Solution deadlock

--- deadlock/deadlock.h
+++ deadlock/deadlock.h
@@ -1,20 +1,27 @@
 #pragma once
 
+#include <thread>
+#include <mutex>
+#include <chrono>
 
 class Deadlock {
- public:
-  Deadlock() {
-  }
+public:
+    Deadlock() {
+    }
 
-  void ThreadOne() {
-    // Your
-  }
+    void ThreadOne() {
+        std::unique_lock lock1(mtx1_);
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::unique_lock lock2(mtx2_);
+    }
 
-  void ThreadTwo() {
-    // Your
-  }
+    void ThreadTwo() {
+        std::unique_lock lock1(mtx2_);
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::unique_lock lock2(mtx1_);
+    }
 
- private:
-  // Your
+private:
+    std::mutex mtx1_;
+    std::mutex mtx2_;
 };
-
