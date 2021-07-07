47669a0e96e2caf33cf055965d95aea57f18e762
Aliya Zaripova <zaripova.aliyah@gmail.com>
2021-07-07 22:40:44 +0300

    Solution ticket_lock

--- ticket_lock/ticket_lock.h
+++ ticket_lock/ticket_lock.h
@@ -1,20 +1,27 @@
 #pragma once
 
+#include <cstddef>
+#include <thread>
+#include <atomic>
+
 
 class TicketLock {
- public:
-  TicketLock() {
-  }
+public:
+    TicketLock() {
+    }
 
-  void Lock() {
-    // Your code
-  }
+    void Lock() {
+        size_t my_ticket = next_ticket_.fetch_add(1);
+        while (now_serving_ != my_ticket) {
+            std::this_thread::yield();
+        }
+    }
 
-  void Unlock() {
-    // Your code
-  }
+    void Unlock() {
+        now_serving_.fetch_add(1);
+    }
 
- private:
-  // Your code
+private:
+    std::atomic<size_t> now_serving_{0};
+    std::atomic<size_t> next_ticket_{0};
 };
-
