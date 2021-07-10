f42eb5285be96f61e68d5de676ce977cf2d4d08d
Aliya Zaripova <zaripova.aliyah@gmail.com>
2021-07-10 12:56:51 +0300

    Solution rw_spin_lock

--- rw_spin_lock/rw_spin_lock.h
+++ rw_spin_lock/rw_spin_lock.h
@@ -1,28 +1,51 @@
 #pragma once
 
+#include <atomic>
+#include <thread>
 
 class RWSpinLock {
- public:
-  RWSpinLock() {
-  }
+public:
+    RWSpinLock() {
+        counter_ = 0;
+    }
 
-  void LockRead() {
-    // Your code
-  }
+    void LockRead() {
+        for (;;) {
+            uint16_t local = counter_.load();
+            if (local & 1) {
+                std::this_thread::yield();
+                continue;
+            }
+            if (counter_.compare_exchange_weak(local, local + 2)) {
+                break;
+            }
+        }
+    }
 
-  void UnlockRead() {
-    // Your code
-  }
+    void UnlockRead() {
+        counter_ -= 2;
+    }
 
-  void LockWrite() {
-    // Your code
-  }
+    void LockWrite() {
+        for (;;) {
+            uint16_t local = counter_.load();
+            if (local & 1) {
+                std::this_thread::yield();
+                continue;
+            }
+            if (counter_.compare_exchange_weak(local, local + 1)) {
+                break;
+            }
+        }
+        while (counter_ != 1) {
+            std::this_thread::yield();
+        }
+    }
 
-  void UnlockWrite() {
-    // Your code
-  }
+    void UnlockWrite() {
+        counter_ -= 1;
+    }
 
- private:
-  // Your code
+private:
+    std::atomic<uint16_t> counter_;
 };
-
