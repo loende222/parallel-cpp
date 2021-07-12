231990374ec01b38666e21cdeb49fcc126b3a613
Aliya Zaripova <zaripova.aliyah@gmail.com>
2021-07-12 10:58:41 +0300

    Solution mpmc_queue

--- mpmc_queue/mpmc_queue.h
+++ mpmc_queue/mpmc_queue.h
@@ -2,26 +2,74 @@
 
 #include <optional>
 #include <cstdint>
-
+#include <atomic>
+#include <vector>
+#include <thread>
 
 template <class T>
 class MPMCQueue {
- public:
-  explicit MPMCQueue(uint64_t /*max_size*/) {
-    // Your code
-  }
+public:
+    struct Node {
+        T value;
+        std::atomic<uint64_t> gen;
+    };
 
-  bool Push(const T& /*value*/) {
-    // Your code
-    return false;
-  }
+    explicit MPMCQueue(uint64_t max_size) {
+        mpmcq_ = new Node[max_size];
+        for (uint64_t i = 0; i != max_size; ++i) {
+            mpmcq_[i].gen.store(i);
+        }
+        size_ = max_size;
+        head_ = 0;
+        tail_ = 0;
+    }
 
-  std::optional<T> Pop() {
-    // Your code
-    return std::nullopt;
-  }
+    ~MPMCQueue() {
+        delete [] mpmcq_;
+    }
 
- private:
-  // Your code
+    bool Push(const T& value) {
+        for (;;) {
+            auto cur_head = head_.load();
+            auto idx = cur_head & (size_ - 1);
+
+            if (cur_head == tail_.load() + size_) {
+                return false;
+            }
+
+            if (head_.compare_exchange_weak(cur_head,  cur_head + 1)) {
+                mpmcq_[idx].value = value;
+                ++mpmcq_[idx].gen;
+                return true;
+            }
+        }
+    }
+
+    std::optional<T> Pop() {
+        for (;;) {
+            auto cur_tail = tail_.load();
+            auto idx = cur_tail & (size_ - 1);
+
+            if (cur_tail == head_.load()) {
+                return std::nullopt;
+            }
+
+            if (mpmcq_[idx].gen.load() != cur_tail + 1) {
+                std::this_thread::yield();
+                continue;
+            }
+
+            if (tail_.compare_exchange_weak(cur_tail, cur_tail + 1)) {
+                auto res = mpmcq_[idx].value;
+                mpmcq_[idx].gen += size_ - 1;
+                return res;
+            }
+        }
+    }
+
+public:
+    Node* mpmcq_;
+    size_t size_;
+    std::atomic<size_t> head_;
+    std::atomic<size_t> tail_;
 };
-
