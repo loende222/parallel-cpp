b52ecf052af322e103b6cceb47472e4d84f4b07d
Aliya Zaripova <zaripova.aliyah@gmail.com>
2021-07-12 12:28:03 +0300

    Resolved reduce

--- reduce/reduce.h
+++ reduce/reduce.h
@@ -10,20 +10,18 @@ template <typename RandomAccessIterator, class T, class Func>
 T parallel_reduce(RandomAccessIterator begin, RandomAccessIterator end,
                   const T& initial_value, Func func, size_t n_threads) {
     std::vector<std::thread> threads;
-    std::atomic<T> result(initial_value);
+    std::vector<T> local_results(n_threads);
 
     for (size_t i = 0; i != n_threads; ++i) {
         RandomAccessIterator from = begin + (end - begin) * i / n_threads;
         RandomAccessIterator to = begin + (end - begin) * (i + 1) / n_threads;
-        threads.emplace_back([from, to, &result, &func] {
-            T local_result = *from;
-            local_result = std::reduce(from + 1, to, local_result, func);
-            result.store(func(result.load(), local_result));
+        threads.emplace_back([from, to, &func, i, initial_value, &local_results] {
+            local_results[i] = std::reduce(from, to, initial_value, func);
         });
     }
 
     for (auto& t : threads) {
         t.join();
     }
-    return result.load();
+    return std::reduce(local_results.begin(), local_results.end(), initial_value, func);
 }
