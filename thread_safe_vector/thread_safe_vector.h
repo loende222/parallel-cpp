d6d98eb9fa40be002a6cef7b95be8bdabf265615
Aliya Zaripova <zaripova.aliyah@gmail.com>
2021-07-12 12:21:33 +0300

    Solution thread_safe_vector

--- thread_safe_vector/thread_safe_vector.h
+++ thread_safe_vector/thread_safe_vector.h
@@ -1,31 +1,40 @@
 #pragma once
 
 #include <vector>
-
+#include <mutex>
+#include <shared_mutex>
+#include <thread>
 
 template <typename T>
 class ThreadSafeVector {
- public:
-  ThreadSafeVector() {
-  }
+public:
+    ThreadSafeVector() = default;
 
-  T operator[](size_t index) const {
-    // Your code
-    return vector_[index];
-  }
+    T operator[](size_t index) const {
+        std::shared_lock lock(mtx1_);
+        return vector_[index];
+    }
 
-  size_t Size() const {
-    // Your code
-    return vector_.size();
-  }
+    size_t Size() const {
+        std::shared_lock lock(mtx1_);
+        return vector_.size();
+    }
 
-  void PushBack(const T& value) {
-    // Your code
-    vector_.push_back(value);
-  }
+    void PushBack(const T& value) {
+        if (vector_.size() == vector_.capacity()) {
+            std::unique_lock lock1(mtx1_);
+            std::unique_lock lock2(mtx2_);
+            vector_.push_back(value);
+        } else {
+            std::shared_lock lock1(mtx1_);
+            std::unique_lock lock2(mtx2_);
+            vector_.push_back(value);
+        }
+    }
 
- private:
-  // Your code
-  std::vector<T> vector_;
+private:
+    std::vector<T> vector_;
+    mutable std::shared_mutex mtx1_;
+    std::mutex mtx2_;
 };
 
