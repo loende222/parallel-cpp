cb6a7d2b235d0fd8586b695a3a3031a0eb94b93f
Aliya Zaripova <zaripova.aliyah@gmail.com>
2021-07-29 17:45:03 +0300

    Resolved thread_safe_vector

--- thread_safe_vector/thread_safe_vector.h
+++ thread_safe_vector/thread_safe_vector.h
@@ -11,30 +11,28 @@ public:
     ThreadSafeVector() = default;
 
     T operator[](size_t index) const {
-        std::shared_lock lock(mtx1_);
+        std::shared_lock r_lock(read_mtx_);
         return vector_[index];
     }
 
     size_t Size() const {
-        std::shared_lock lock(mtx1_);
+        std::shared_lock r_lock(read_mtx_);
         return vector_.size();
     }
 
     void PushBack(const T& value) {
+        std::unique_lock w_lock(write_mtx_);
         if (vector_.size() == vector_.capacity()) {
-            std::unique_lock lock1(mtx1_);
-            std::unique_lock lock2(mtx2_);
+            std::unique_lock r_lock(read_mtx_);
             vector_.push_back(value);
         } else {
-            std::shared_lock lock1(mtx1_);
-            std::unique_lock lock2(mtx2_);
             vector_.push_back(value);
         }
     }
 
 private:
     std::vector<T> vector_;
-    mutable std::shared_mutex mtx1_;
-    std::mutex mtx2_;
+    mutable std::shared_mutex read_mtx_;
+    std::mutex write_mtx_;
 };
 
